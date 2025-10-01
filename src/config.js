c548c300227175cccc2c3dd2ee67dff94798510c
Dennis0101 <gamehack1346@gmail.com>
2025-10-01 18:26:19 +0900

    Update and rename config.js to config.ts

--- src/config.js
+++
@@ -1,6 +0,0 @@
-export const CFG = {
-  DAILY_BONUS: Number(process.env.CASINO_DAILY_BONUS ?? 500),
-  BJ_BET_SEC:  Number(process.env.BLACKJACK_BET_SECONDS ?? 20),
-  BJ_ACT_SEC:  Number(process.env.BLACKJACK_ACTION_SECONDS ?? 15),
-  BAC_BET_SEC: Number(process.env.BACCARAT_BET_SECONDS ?? 15)
-};
